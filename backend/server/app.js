23f6179c9575981b5c3b7fe7143d9be8562d33fa
yasTheDreamer <yassine19265@gmail.com>
2020-11-22 18:32:04 +0100

    fix : remove security alert by removing handlebars

--- backend/server/app.js
+++ backend/server/app.js
@@ -8,10 +8,6 @@ import usersRouter from "./routes/users";
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
